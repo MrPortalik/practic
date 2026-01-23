dfa7a13cd415defc0faab627cad0e0a2c77549d4
MrPortalik <galagala301184@gmail.com>
2026-01-23 10:42:33 +1000

    Swiper-slider

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 import {burger} from '/scripts/burger.js';
 import {accordion} from '/scripts/accordion.js';
+import {slider} from '/scripts/slider.js';
 
 document.addEventListener('DOMContentLoaded', () => {
     burger();
     baguetteBox.run('.galleryBox');
+    slider();
     accordion();
 });
