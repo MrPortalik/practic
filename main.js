cdae3eac39dd5f972a71a29b062809ad8e5fdacc
MrPortalik <galagala301184@gmail.com>
2026-01-23 14:39:21 +1000

    Form

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 import {burger} from '/scripts/burger.js';
 import {accordion} from '/scripts/accordion.js';
 import {slider} from '/scripts/slider.js';
+import {form} from '/scripts/form.js';
 
 document.addEventListener('DOMContentLoaded', () => {
     burger();
     baguetteBox.run('.galleryBox');
     slider();
     accordion();
+    form();
 });
---
+++ scripts/form.js
@@ -0,0 +1,23 @@
+export function form() {
+    console.log('Функция работает');
+    const buttons = document.querySelectorAll('.requestBtn');
+    const section = document.querySelector('#form');
+    
+    buttons.forEach(function(button) {
+        button.addEventListener('click', ()=> {
+        section.classList.add('_active');
+        document.body.style.overflow = 'hidden';
+        document.querySelector('body').classList.add('_lock')
+
+        document.addEventListener('click', (event)=> {
+            if (!event.composedPath().includes(button) && 
+            !event.composedPath().includes(document.querySelector('form'))) {
+                    document.body.style.overflow = 'visible';
+                    section.classList.remove('_active');
+                }
+            })
+        })
+    })
+}
+
+//добавить выход из формы по Esc + назначать на утилитарный класс блокировку прокрута
